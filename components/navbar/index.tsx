282ff92ae21151708f8fc0dcdd2cf9fef61967fe
chenzhenmin <1290433700@qq.com>
2022-08-25 11:31:03 +0800

    feat: 弹窗组件实现

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,14 +1,16 @@
-import { FC, useContext } from "react";
+import { FC, useContext, useRef } from "react";
 import styles from "./styles.module.scss";
 import { ThemeContext } from "@/stores/theme";
 import { UserAgentContext } from "@/stores/userAgent";
 import { Themes, Environment } from "@/constants/enum";
+import { Popup, IPopupRef } from "../popup";
 
 export interface INavBarProps {}
 
 export const NavBar: FC<INavBarProps> = ({}) => {
   const { setTheme } = useContext(ThemeContext);
   const { userAgent } = useContext(UserAgentContext);
+  const popupRef = useRef<IPopupRef>(null);
 
   return (
     <div className={styles.navBar}>
@@ -16,6 +18,14 @@ export const NavBar: FC<INavBarProps> = ({}) => {
         <div className={styles.logoIcon}></div>
       </a>
       <div className={styles.themeArea}>
+        <div
+          className={styles.popupText}
+          onClick={(): void => {
+            popupRef.current?.open();
+          }}
+        >
+          弹窗示范
+        </div>
         {userAgent === Environment.pc && (
           <span className={styles.text}>当前是pc端样式</span>
         )}
@@ -36,6 +46,9 @@ export const NavBar: FC<INavBarProps> = ({}) => {
           }}
         ></div>
       </div>
+      <Popup ref={popupRef}>
+        <div>这是一个弹窗</div>
+      </Popup>
     </div>
   );
 };
---
+++ components/popup/index.tsx
@@ -0,0 +1,92 @@
+import React, {
+  forwardRef,
+  useState,
+  useEffect,
+  useImperativeHandle,
+  useContext,
+  useMemo,
+} from "react";
+import styles from "./styles.module.scss";
+import ReactDom from "react-dom";
+import { UserAgentContext } from "@/stores/userAgent";
+import { Environment } from "@/constants/enum";
+import cName from "classnames";
+
+export interface IPopupRef {
+  open: () => void;
+}
+
+interface IProps {
+  children: JSX.Element;
+}
+
+export const Popup = forwardRef<IPopupRef, IProps>(({ children }, ref) => {
+  const [visible, setVisible] = useState(false);
+  const [enter, setEnter] = useState(false);
+  const [leave, setLeave] = useState(false);
+  const { userAgent } = useContext(UserAgentContext);
+
+  const maskClass = useMemo(() => {
+    return userAgent === Environment.mobile ? "forbidScroll" : "pcForbidScroll";
+  }, [userAgent]);
+
+  useEffect(() => {
+    document.body.className = visible ? maskClass : "";
+    let timeout;
+    if (visible) {
+      setEnter(true);
+      timeout = setTimeout((): void => {
+        setEnter(false);
+      }, 300);
+    } else {
+      setLeave(true);
+      timeout = setTimeout((): void => {
+        setLeave(false);
+      }, 300);
+    }
+    return (): void => {
+      timeout = null;
+    };
+  }, [visible]);
+
+  useImperativeHandle(ref, () => ({
+    open: (): void => {
+      setEnter(true);
+      setVisible(true);
+      setTimeout((): void => {
+        setEnter(false);
+      }, 300);
+    },
+  }));
+
+  const renderDom = visible ? (
+    <div
+      className={cName({
+        [styles.popup]: true,
+        [styles.enter]: enter,
+        [styles.leave]: leave,
+      })}
+    >
+      <div className={styles.mask} />
+      <div className={styles.popupContent}>
+        <div
+          className={styles.closeBtn}
+          onClick={(): void => {
+            setLeave(true);
+            setTimeout((): void => {
+              setLeave(false);
+            }, 300);
+            setVisible(false);
+          }}
+        />
+        {children}
+      </div>
+    </div>
+  ) : (
+    <></>
+  );
+
+  return typeof document !== "undefined"
+    ? ReactDom.createPortal(renderDom, document.body)
+    : renderDom;
+});
